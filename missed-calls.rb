086de43fc3520383f1326a31f44ce931bd87855d
ubergesundheit <ubergesundheit@googlemail.com>
2015-01-26 00:48:04 +0100

    nil fix

--- missed-calls.rb
+++ missed-calls.rb
@@ -30,26 +30,28 @@ res = http.request(req)
 parsed_response = Hash.from_xml(res.body)
 
 # WTF xml..
+xml_calls = parsed_response["methodResponse"]["params"]["param"]["value"]["struct"]["member"].select { |h| h["name"] == "History" }[0]["value"]["array"]["data"]["value"]
+unless xml_calls == nil
+  calls = xml_calls.map do |struct|
+    member = struct["struct"]["member"]
+    {
+      timestamp: extract_property("Timestamp", member),
+      caller_id: extract_property("RemoteUri", member)[/\d+|anonymous/]
+    }
+  end
 
-calls = parsed_response["methodResponse"]["params"]["param"]["value"]["struct"]["member"].select { |h| h["name"] == "History" }[0]["value"]["array"]["data"]["value"].map do |struct|
-  member = struct["struct"]["member"]
-  {
-    timestamp: extract_property("Timestamp", member),
-    caller_id: extract_property("RemoteUri", member)[/\d+|anonymous/]
-  }
-end
-
-unless calls.length == 0
-  message = {
-    subject: ENV["EMAIL_SUBJECT"],
-    from_name: ENV["EMAIL_FROM_NAME"],
-    text: calls.map { |e| e.values.join " " }.join("\n"),
-    to: [{
-      email: ENV["EMAIL_TO"],
-      name: ENV["EMAIL_TO_NAME"]
-    }],
-    from_email: ENV["EMAIL_FROM"]
-  }
-  mandrill = Mandrill::API.new ENV["MANDRILL_APIKEY"]
-  mandrill.messages.send message
+  unless calls.length == 0
+    message = {
+      subject: ENV["EMAIL_SUBJECT"],
+      from_name: ENV["EMAIL_FROM_NAME"],
+      text: calls.map { |e| e.values.join " " }.join("\n"),
+      to: [{
+        email: ENV["EMAIL_TO"],
+        name: ENV["EMAIL_TO_NAME"]
+      }],
+      from_email: ENV["EMAIL_FROM"]
+    }
+    mandrill = Mandrill::API.new ENV["MANDRILL_APIKEY"]
+    mandrill.messages.send message
+  end
 end
